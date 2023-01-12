979a757547d3c8de59dcdea9b374117ebd14385b
CrackInSpace <115029317+CrackInSpace@users.noreply.github.com>
2023-01-12 14:41:18 +0300

    Для собеса: Фибонначи, факториал, бинарный поиск, жадные алгоритмы, задачка про рюкзак

--- Algoritms/GreedyAlgorithms/GreedyAlgorithms2.java
+++ Algoritms/GreedyAlgorithms/GreedyAlgorithms2.java
@@ -13,13 +13,14 @@ public class GreedyAlgorithms2 {
         int stops = 0;
 
         for (int i = 0; i < stations.length - 1; i++) {
-            //System.out.println(stations[i + 1]); Проверка станций
+            //System.out.println(stations[i + 1]); //Проверка станций
             nextFuel = stations[i + 1] - stations[i];
             if (currentFuel - nextFuel > 0) {
                 currentFuel -= nextFuel;
             } else {
                 stops++;
                 currentFuel = fuelCapacity;
+                a--;//Чтобы не перескакивать на следующую не отняв текущее
             }
         }
         return stops;
